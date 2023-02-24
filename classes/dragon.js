ceacac19c1c9a08963889549639bc1c72e80a4db
Sujal Shrestha <84851820+SuzalShrestha@users.noreply.github.com>
2023-02-24 04:07:22 +0000

    Completed

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,24 +1,22 @@
 // Your code here
-class Dragon{
-  constructor(name,color){
-    this.name=name;
-    this.color=color;
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
 
   }
-  breathesFire(){
+  breathesFire() {
     return `${this.name} breathes fire everywhere! BURN!!!!`
   }
-  static getDragons(...dragons){
-    let arr=[];
-    for(let dragon of dragons){
+  static getDragons(...dragons) {
+    let arr = [];
+    for (let dragon of dragons) {
       arr.push(dragon.name);
     }
     return arr;
   }
 }
-const puff = new Dragon("Puff", "green");
-console.log(puff);
-console.log(puff.breathesFire());
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
